6066d61468c0049f92f8cbe3114675d4a19640f7
Maso79 <67212858+maso79@users.noreply.github.com>
2021-09-27 12:36:46 +0200

    klkk

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
-const schemaUser = new mongoose.Schema({
-  nome: {
-    type: String,
-    required: true,
-    unique: true,
+const schemaUser = new mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true,
+    },
+    cognome: {
+      type: String,
+      required: true,
+    },
   },
-  cognome: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-},{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-const User=mongoose.model("User",schemaUser,"utenti")
-module.exports = User
+const User = mongoose.model("User", schemaUser, "utenti");
+module.exports = User;
